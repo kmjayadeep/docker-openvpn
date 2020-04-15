d3d8b9f6f93a72acfe62d3ff4cd72291127f47ad
Jayadeep KM <kmjayadeep@gmail.com>
2020-04-15 23:22:01 +0530

    make init script configurable

--- init.sh
+++ init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-docker-compose run --rm openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM
-docker-compose run --rm openvpn ovpn_initpki
-docker-compose up -d openvpn
+
+if [ -z $1 ]; then
+echo "Initialize openvpn container
+    
+Usage:
+    $(basename "$0") [IP_ADDRESS]
+    "
+    exit 0
+else
+    echo "Setting up openvpn for $1"; fi
+
+docker-compose run --rm openvpn ovpn_genconfig -u udp://$1
+# docker-compose run --rm openvpn ovpn_initpki
+# docker-compose up -d openvpn
